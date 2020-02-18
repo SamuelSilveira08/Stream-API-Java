607a46fc13659754890ee1921a62c6390fd1b070
Samuel Silveira <samuel.santossilveira08@gmail.com>
2020-02-18 16:38:35 -0300

    Done Function and BinaryOperator

--- Stream-API/src/br/com/streams/MainFunctionalInterfaces.java
+++ Stream-API/src/br/com/streams/MainFunctionalInterfaces.java
@@ -1,6 +1,8 @@
 package br.com.streams;
 
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class MainFunctionalInterfaces {
 
@@ -10,8 +12,23 @@ public class MainFunctionalInterfaces {
 		Consumer<Empregado> consumer = emp -> {
 			System.out.println(emp.getNome() + ", R$ " + emp.getSalario());
 		};
-
+		
 		consumer.accept(new Empregado(10, "Samuel", 3000, "TI"));
+		
+		// Entram dois parâmetros e retorna algo
+		
+		Function<Empregado, Double> function = emp -> emp.getSalario() * 10;
+		
+		double salaryTenTimes = function.apply(new Empregado (10, "Samuel", 3000, "TI"));
+		
+		System.out.println(salaryTenTimes);
+		
+		// 
+		
+		BinaryOperator<Empregado> binaryOperator = (emp1, emp2) -> new Empregado(5, emp1.getNome() + emp2.getNome(),
+				emp1.getSalario() + emp2.getSalario(), "Junção");
+		
+		System.out.println(binaryOperator.apply(new Empregado(10, "Samuel", 3000, "TI"), new Empregado(15, "Samuel", 3000, "TI")));
 	}
 
 }
