0e0f05132302a09f342466af6bcafe37c4e3b455
Samuel Silveira <samuel.santossilveira08@gmail.com>
2020-02-18 16:26:08 -0300

    Done consumer

--- Stream-API/src/br/com/streams/MainFunctionalInterfaces.java
+++ Stream-API/src/br/com/streams/MainFunctionalInterfaces.java
@@ -1,5 +1,17 @@
 package br.com.streams;
 
+import java.util.function.Consumer;
+
 public class MainFunctionalInterfaces {
+
+	public static void main(String[] args) {
+
+		// Entra um parâmetro e não retorna nada
+		Consumer<Empregado> consumer = emp -> {
+			System.out.println(emp.getNome() + ", R$ " + emp.getSalario());
+		};
+
+		consumer.accept(new Empregado(10, "Samuel", 3000, "TI"));
+	}
 
 }
